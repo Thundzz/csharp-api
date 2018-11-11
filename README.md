1b2c081593e068e0e92d265b088fb74c40b58331
Thundzz <thundzz@gmail.com>
2018-11-11 15:22:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# A bare-bones C# API template.
+
+This template has been created on windows, using Visual Studio 2017 Community Edition.
+
+The API exposes two routes : a simple ping and a method that returns the current time.
+
---
+++ SimpleApi/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using System.Web.Http.Cors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System.Threading;
+using System;
+
+namespace SimpleApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebApp.Start<Startup>("http://localhost:9001/Hello/");
+            Console.ReadLine();
+        }
+    }
+
+    [RoutePrefix("api")]
+    public class MyController : ApiController
+    {
+        [HttpGet]
+        [Route("ping")]
+        public string Ping()
+        {
+            return "pong";
+        }
+
+        [HttpGet]
+        [Route("time")]
+        public string Pong()
+        {
+            return DateTime.Now.ToString();
+        }
+
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var configuration = new HttpConfiguration();
+
+            configuration.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            configuration.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            configuration.MapHttpAttributeRoutes();
+            configuration.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            
+            appBuilder.UseWebApi(configuration);
+        }
+    }
+
+}
